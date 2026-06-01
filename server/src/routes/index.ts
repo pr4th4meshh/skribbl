93c8baba7caeddad733429e8850f194bad08cd3a
pr4th4meshh <prathameshasolkar17@gmail.com>
2026-06-01 16:17:34 +0530

    fix: add room and leaderboard router inside index router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import authRouter from '../services/auth/router/auth.router';
+import roomRouter from '../services/room/router/room.router';
+import leaderboardRouter from '../services/leaderboard/router/leaderboard.router';
 
 const router = Router();
 
 router.use('/auth', authRouter);
+router.use('/rooms', roomRouter);
+router.use('/leaderboard', leaderboardRouter);
 
 export default router;
