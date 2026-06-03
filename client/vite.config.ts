6b71e846350230e09eaaf0b24ea5ef4efead2aa2
pr4th4meshh <prathameshasolkar17@gmail.com>
2026-06-03 22:18:29 +0530

    feat: start with frontend and add dependencies

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,22 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
   ],
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') },
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': { target: 'http://localhost:6969', changeOrigin: true },
+    },
+  },
 })
